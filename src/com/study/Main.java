a7389b2dfb0747e4782076842e0d8e6fec36a325
libaoyin <penhuoyuer@163.com>
2022-06-29 20:30:33 +0800

    Initial commit

---
+++ src/com/study/Main.java
@@ -0,0 +1,11 @@
+package com.study;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Runnable r = ()-> System.out.println("无参，只有一行代码，无返回值");
+        r.run();
+
+    }
+}
